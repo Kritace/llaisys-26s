ac789a6dd83ba986922396cf0bf05bc6c1aa6a2e
Kritace <2928665377@qq.com>
2026-07-26 01:11:53 +0800

    complete homework #2 rms_norm part

--- src/ops/rms_norm/op.cpp
+++ src/ops/rms_norm/op.cpp
@@ -1,7 +1,37 @@
 #include "op.hpp"
 
+#include "../../core/llaisys_core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/rms_norm_cpu.hpp"
+
 namespace llaisys::ops {
 void rms_norm(tensor_t out, tensor_t in, tensor_t weight, float eps) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, in, weight);
+    CHECK_SAME_DTYPE(out->dtype(), in->dtype());
+    CHECK_SAME_DTYPE(out->dtype(), weight->dtype());
+
+    ASSERT(out->isContiguous() && in->isContiguous() && weight->isContiguous(),
+           "RMSNorm: all tensors must be contiguous.");
+
+    // always support cpu calculation
+    if (out->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::rms_norm(out, in, weight, out->dtype(), eps);
+    }
+
+    llaisys::core::context().setDevice(in->deviceType(), out->deviceId());
+
+    switch (in->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::rms_norm(out, in, weight, out->dtype(), eps);
+
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
