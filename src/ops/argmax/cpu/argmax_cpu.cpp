876f22020e9c341711320ec3387caebc724a1728
Kritace <2928665377@qq.com>
2026-07-25 22:03:25 +0800

    complete homework #2 embedding part

--- src/ops/argmax/cpu/argmax_cpu.cpp
+++ src/ops/argmax/cpu/argmax_cpu.cpp
@@ -36,19 +36,19 @@ void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals, llaisysDataType_t
     switch (type) {
     case LLAISYS_DTYPE_F32:
         return argmax_(reinterpret_cast<const float *>(vals->data()), 
-        reinterpret_cast<float *>(max_val->data()), 
-        reinterpret_cast<std::int64_t *>(max_idx->data()), 
-        numel);
+                       reinterpret_cast<float *>(max_val->data()), 
+                       reinterpret_cast<std::int64_t *>(max_idx->data()), 
+                       numel);
     case LLAISYS_DTYPE_BF16:
         return argmax_(reinterpret_cast<const llaisys::bf16_t *>(vals->data()), 
-        reinterpret_cast<llaisys::bf16_t *>(max_val->data()), 
-        reinterpret_cast<std::int64_t *>(max_idx->data()), 
-        numel);
+                       reinterpret_cast<llaisys::bf16_t *>(max_val->data()), 
+                       reinterpret_cast<std::int64_t *>(max_idx->data()), 
+                       numel);
     case LLAISYS_DTYPE_F16:
         return argmax_(reinterpret_cast<const llaisys::fp16_t *>(vals->data()), 
-        reinterpret_cast<llaisys::fp16_t *>(max_val->data()), 
-        reinterpret_cast<std::int64_t *>(max_idx->data()), 
-        numel);
+                       reinterpret_cast<llaisys::fp16_t *>(max_val->data()), 
+                       reinterpret_cast<std::int64_t *>(max_idx->data()), 
+                       numel);
     default:
         EXCEPTION_UNSUPPORTED_DATATYPE(type);
     }
--- src/ops/embedding/cpu/embedding_cpu.hpp
+++ src/ops/embedding/cpu/embedding_cpu.hpp
@@ -2,10 +2,8 @@
 #include "llaisys.h"
 #include "../../../tensor/tensor.hpp"
 
-#include <cstddef>
-
 using tensor_t = llaisys::tensor_t;
 
 namespace llaisys::ops::cpu {
-void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals, llaisysDataType_t type, size_t numel);
+void embedding(tensor_t out, tensor_t index, tensor_t weight, llaisysDataType_t type, size_t num_rows, size_t num_cols);
 } // namespace llaisys::ops::cpu
