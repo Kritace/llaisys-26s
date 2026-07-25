39c898387c136777d9873f0f0b2c94f53046fc18
Kritace <2928665377@qq.com>
2026-07-26 03:29:33 +0800

    complete homework #2 rope part

--- src/ops/rope/op.cpp
+++ src/ops/rope/op.cpp
@@ -1,7 +1,35 @@
 #include "op.hpp"
 
+#include "../../core/llaisys_core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/rope_cpu.hpp"
+
 namespace llaisys::ops {
 void rope(tensor_t out, tensor_t in, tensor_t pos_ids, float theta) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, in, pos_ids);
+    CHECK_SAME_DTYPE(out->dtype(), in->dtype());
+    ASSERT(out->isContiguous() && in->isContiguous() && pos_ids->isContiguous(),
+           "Rope: all tensors must be contiguous.");
+    
+    // always support cpu calculation
+    if (out->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::rope(out, in, pos_ids, out->dtype(), theta);
+    }
+
+    llaisys::core::context().setDevice(in->deviceType(), out->deviceId());
+
+    switch (in->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::rope(out, in, pos_ids, out->dtype(), theta);
+
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
