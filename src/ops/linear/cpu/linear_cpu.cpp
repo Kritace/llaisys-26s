7dfc71e7aa72bfee22fa8d2c5f9a52e85d97f6b8
Kritace <2928665377@qq.com>
2026-07-25 23:19:49 +0800

    complete homework #2 linear part

---
+++ src/ops/linear/cpu/linear_cpu.cpp
@@ -0,0 +1,54 @@
+#include "linear_cpu.hpp"
+#include "../../add/cpu/add_cpu.hpp"
+
+#include "../../../utils.hpp"
+#include <iostream>
+
+// 线性变换操作：计算 out = in * weight^T + bias, bias是可选的。
+template <typename T>
+void linear_(const T *in, const T *weight, T *out, size_t in_rows, size_t in_cols, size_t out_cols, const T *bias = nullptr) {
+
+    for (size_t i = 0; i < in_rows; ++i) {
+        for (size_t j = 0; j < out_cols; ++j) {
+            float acc = 0.0f;
+            for (size_t k = 0; k < in_cols; ++k) {
+                float a = llaisys::utils::cast<float>(in[i * in_cols + k]);
+                float b = llaisys::utils::cast<float>(weight[j * in_cols + k]);
+                acc += a * b;
+            }
+            if (bias) acc += llaisys::utils::cast<float>(bias[j]);
+            out[i * out_cols + j] = llaisys::utils::cast<T>(acc);
+        }
+    }
+}
+
+namespace llaisys::ops::cpu {
+void linear(tensor_t out, tensor_t in, tensor_t weight, tensor_t bias, llaisysDataType_t type, size_t /*numel*/) {
+    const size_t in_rows = in->shape()[0]; // Batch size
+    const size_t in_cols = in->shape()[1]; // Input features
+    const size_t out_cols = weight->shape()[0]; // Output features
+
+    switch (type) {
+    case LLAISYS_DTYPE_F32:
+        return linear_(reinterpret_cast<const float *>(in->data()),
+                       reinterpret_cast<const float *>(weight->data()),
+                       reinterpret_cast<float *>(out->data()),
+                       in_rows, in_cols, out_cols,
+                       bias ? reinterpret_cast<const float *>(bias->data()) : nullptr);
+    case LLAISYS_DTYPE_BF16:
+        return linear_(reinterpret_cast<const llaisys::bf16_t *>(in->data()),
+                       reinterpret_cast<const llaisys::bf16_t *>(weight->data()),
+                       reinterpret_cast<llaisys::bf16_t *>(out->data()),
+                       in_rows, in_cols, out_cols,
+                       bias ? reinterpret_cast<const llaisys::bf16_t *>(bias->data()) : nullptr);
+    case LLAISYS_DTYPE_F16:
+        return linear_(reinterpret_cast<const llaisys::fp16_t *>(in->data()),
+                       reinterpret_cast<const llaisys::fp16_t *>(weight->data()),
+                       reinterpret_cast<llaisys::fp16_t *>(out->data()),
+                       in_rows, in_cols, out_cols,
+                       bias ? reinterpret_cast<const llaisys::fp16_t *>(bias->data()) : nullptr);
+    default:
+        EXCEPTION_UNSUPPORTED_DATATYPE(type);
+    }
+}
+} // namespace llaisys::ops::cpu
--- src/ops/linear/op.cpp
+++ src/ops/linear/op.cpp
@@ -1,7 +1,35 @@
 #include "op.hpp"
 
+#include "../../core/llaisys_core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/linear_cpu.hpp"
+
 namespace llaisys::ops {
 void linear(tensor_t out, tensor_t in, tensor_t weight, tensor_t bias) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, in, weight, bias);
+    CHECK_SAME_DTYPE(out->dtype(), in->dtype(), weight->dtype(), bias->dtype());
+    ASSERT(out->isContiguous() && in->isContiguous() && weight->isContiguous() && bias->isContiguous(),
+           "Linear: all tensors must be contiguous.");
+
+    // always support cpu calculation
+    if (out->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::linear(out, in, weight, bias, in->dtype(), in->numel());
+    }
+
+    llaisys::core::context().setDevice(in->deviceType(), out->deviceId());
+
+    switch (in->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::linear(out, in, weight, bias, in->dtype(), in->numel());
+
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
