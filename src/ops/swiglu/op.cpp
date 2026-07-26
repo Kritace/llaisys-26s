ec56bc46a8ed136473c76af25f5e7a8cf665f4e1
Kritace <2928665377@qq.com>
2026-07-26 21:26:07 +0800

    complete swiglu

--- src/ops/swiglu/op.cpp
+++ src/ops/swiglu/op.cpp
@@ -1,7 +1,34 @@
 #include "op.hpp"
 
+#include "../../core/llaisys_core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/swiglu_cpu.hpp"
+
 namespace llaisys::ops {
 void swiglu(tensor_t out, tensor_t gate, tensor_t up) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, gate, up);
+    CHECK_SAME_DTYPE(gate->dtype(), up->dtype());
+    ASSERT(out->isContiguous() && gate->isContiguous() && up->isContiguous(),
+           "SwiGLU: all tensors must be contiguous.");
+
+    if (up->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::swiglu(out, gate, up, gate->dtype(), gate->numel());
+    }
+
+    llaisys::core::context().setDevice(gate->deviceType(), gate->deviceId());
+
+    switch (up->deviceType()){
+        case LLAISYS_DEVICE_CPU:
+            return cpu::swiglu(out, gate, up, gate->dtype(), gate->numel());
+
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
